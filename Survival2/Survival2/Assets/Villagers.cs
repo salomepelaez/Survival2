84224b21d890ce38b537f64fce6355419c5d567f
Salomé Pelaez <spsalome@gmail.com>
2019-08-05 17:39:44 -0500

    Adds Villagers class

---
+++ Survival2/Survival2/Assets/Villagers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villagers : MonoBehaviour
+{
+    void Start()
+    {
+        transform.name = "Villager";
+        GameObject Villager = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Villager.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    
+}
