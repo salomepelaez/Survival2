d102f0becdb4efc7765c9bdca07b67a3a25a4416
Salomé Pelaez <spsalome@gmail.com>
2019-08-05 17:38:44 -0500

    Adds Hero class

---
+++ Survival2/Survival2/Assets/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public GameObject held;
+
+    void Start()
+    {
+        GameObject s = Instantiate(held, Vector3.zero, Quaternion.identity);
+        
+        s.GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.name == "Villager")
+        {
+            Debug.Log("miau");
+        }
+
+        if (collision.transform.name == "Zombie")
+        {
+            Debug.Log("guau");
+        }
+    }
+}
+
+//Ya no crea el héroe :(
