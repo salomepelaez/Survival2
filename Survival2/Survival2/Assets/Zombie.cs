d5e568435adade1dc889acbf8ccf558f46a4c938
Salomé Pelaez <spsalome@gmail.com>
2019-08-05 14:58:25 -0500

    Adds Zombie class

---
+++ Survival2/Survival2/Assets/Zombie.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{           
+    public void Start()
+    {     
+       
+        /*
+         
+         * Este bloque de código se encarga de crear un objeto primitivo, en este caso un cubo.
+         * También le asigna una ubicación aleatoria utilizando un random y un Vector3.
+         * También le asigna un color aleatorio y un mensaje dependiendo de este.
+         
+        */
+
+        GameObject zZombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        int color = Random.Range(0, 3);
+
+        switch (color)
+        {
+            case 0:
+                zZombie.GetComponent<Renderer>().material.color = Color.cyan;
+                break;
+            case 1:
+                zZombie.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case 2:
+                zZombie.GetComponent<Renderer>().material.color = Color.magenta;
+                break;
+        }
+
+        
+    }
+
+    struct ZombieData
+    {
+        public enum MyTaste
+        {
+            Brains,
+            Hearts,
+            Eyes,
+            Ears,
+            Mouths
+        }
+
+        public enum Move
+        {
+            Idle,
+            Moving
+        }
+    }
+}
+
+
