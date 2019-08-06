9d0e637448c47a5504231e067c68a3d508f29842
Salomé Peláez <spsalome@gmail.com>
2019-08-06 15:58:34 -0500

    Changes Zombie class

--- Survival2/Survival2/Assets/Zombie.cs
+++ Survival2/Survival2/Assets/Zombie.cs
@@ -3,46 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Zombie : MonoBehaviour
-{           
+{
+    public enum MyTaste
+    {
+        Brains,
+        Hearts,
+        Eyes,
+        Ears,
+        Mouths
+    }
+
+    public enum Move
+    {
+        Idle,
+        Moving
+    }
+
+    public MyTaste taste1;
+    public MyTaste taste2;
+    public MyTaste taste3;
+    public MyTaste taste4;
+    public MyTaste taste5;
+
     public void Start()
-    {  
-        GameObject zZombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        
+    {               
         int color = Random.Range(0, 3);
 
         switch (color)
         {
             case 0:
-                zZombie.GetComponent<Renderer>().material.color = Color.cyan;
+                GetComponent<Renderer>().material.color = Color.cyan;
                 break;
             case 1:
-                zZombie.GetComponent<Renderer>().material.color = Color.green;
+                GetComponent<Renderer>().material.color = Color.green;
                 break;
             case 2:
-                zZombie.GetComponent<Renderer>().material.color = Color.magenta;
+                GetComponent<Renderer>().material.color = Color.magenta;
                 break;
         }
 
+        int tastes = Random.Range(0, 6);
+
+        switch (tastes)
+        {
+            case 0:
+                taste1 = MyTaste.Brains;
+                string cerebros = taste1; //Imprimir el gusto
+                break;
+
+            case 1:
+                taste2 = MyTaste.Hearts;
+
+                break;
+
+            case 2:
+                taste3 = MyTaste.Eyes;
+                break;
+
+            case 3:
+                taste4 = MyTaste.Ears;
+                break;
+
+            case 4:
+                taste5 = MyTaste.Mouths;
+                break;
+
+        }
         transform.name = "Zombie";
+
+        Debug.Log("Waaaarr quiero comer" + (tastes));
     }
 
-    struct ZombieData
-    {
-        public enum MyTaste
-        {
-            Brains,
-            Hearts,
-            Eyes,
-            Ears,
-            Mouths
-        }
+    
+}
 
-        public enum Move
-        {
-            Idle,
-            Moving
-        }
-    }
-} 
+
+       
+        
+        
+        
+  
+
 
 
