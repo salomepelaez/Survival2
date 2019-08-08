4aed18a35d465fecb13496edf939a68724fa8203
Salomé Pelaez <spsalome@gmail.com>
2019-08-08 16:26:28 -0500

    Changes Zombie class

--- Survival2/Survival2/Assets/Zombie.cs
+++ Survival2/Survival2/Assets/Zombie.cs
@@ -3,27 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Zombie : MonoBehaviour
-{
-    public enum MyTaste
-    {
-        Brains,
-        Hearts,
-        Eyes,
-        Ears,
-        Mouths
-    }
-
-    public enum Move
-    {
-        Idle,
-        Moving
-    }
-
-    public MyTaste taste1;
-    public MyTaste taste2;
-    public MyTaste taste3;
-    public MyTaste taste4;
-    public MyTaste taste5;
+{      
 
     public void Start()
     {               
@@ -41,48 +21,31 @@ public class Zombie : MonoBehaviour
                 GetComponent<Renderer>().material.color = Color.magenta;
                 break;
         }
+            
 
-        int tastes = Random.Range(0, 6);
+        transform.name = "Zombie";        
+    }    
+}
 
-        switch (tastes)
-        {
-            case 0:
-                taste1 = MyTaste.Brains;
-                string cerebros = taste1; //Imprimir el gusto
-                break;
+public enum MyTaste
+{
+    Cerebros,
+    Corazones,
+    Ojos,
+    Orejas,
+    Bocas
+}
 
-            case 1:
-                taste2 = MyTaste.Hearts;
-
-                break;
-
-            case 2:
-                taste3 = MyTaste.Eyes;
-                break;
-
-            case 3:
-                taste4 = MyTaste.Ears;
-                break;
-
-            case 4:
-                taste5 = MyTaste.Mouths;
-                break;
-
-        }
-        transform.name = "Zombie";
-
-        Debug.Log("Waaaarr quiero comer" + (tastes));
-    }
-
-    
+public enum Move
+{
+    Idle,
+    Moving
 }
 
 
-       
-        
-        
-        
-  
+
+
+
 
 
 
