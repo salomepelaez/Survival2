22e105e80582ef74be0dc35a113014adf073114b
Salomé Pelaez <spsalome@gmail.com>
2019-08-13 17:30:57 -0500

    Chnges HeroAim class

--- Survival2/Survival2/Assets/HeroAim.cs
+++ Survival2/Survival2/Assets/HeroAim.cs
@@ -6,7 +6,9 @@ public class HeroAim : MonoBehaviour
 {
     float mouseX;
 	float mouseY;
-	float sensitivity = 100.0f;
+	float sensitivity = 40.0f;
+	float xAxisClamp = 0.0f;
+
     void Update()
     {
         CameraRot();
@@ -17,8 +19,34 @@ public class HeroAim : MonoBehaviour
 	{
 		 mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
 		 mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+		 xAxisClamp += mouseY;
+
+		 if(xAxisClamp > 90.0f)
+		 {
+			 xAxisClamp = 90.0f;
+			 mouseY = 0.0f;
+			 ClampXAxis(270.0f);
+		 }
+
+		 else if(xAxisClamp < -90.0f)
+		 {
+			 xAxisClamp = -90.0f;
+			 mouseY = 0.0f;
+			 ClampXAxis(90.0f);
+		 }
 		
-		transform.Rotate(-transform.right * mouseY);
+		transform.Rotate(Vector3.left * mouseY);
+		transform.Rotate(Vector3.up * mouseX);
 		
+		
+	}
+
+	void ClampXAxis(float myX)
+	{
+		Vector3 eulerRotation = transform.eulerAngles;
+		eulerRotation.x = myX;
+		transform.eulerAngles = eulerRotation;
+				
 	}
 }
