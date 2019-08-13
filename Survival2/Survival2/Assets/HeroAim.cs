da0abafb67712f670803607a6a52267ae8ab172e
Salomé Pelaez <spsalome@gmail.com>
2019-08-13 15:42:51 -0500

    Changes HeroAim class

--- Survival2/Survival2/Assets/HeroAim.cs
+++ Survival2/Survival2/Assets/HeroAim.cs
@@ -4,25 +4,21 @@ using UnityEngine;
 
 public class HeroAim : MonoBehaviour
 {
-    // Se crearon dos variables de tipo flotante, que almacenan la ubicación en X y Y del mouse.
     float mouseX;
-    float mouseY;
-    public bool InvertedMouse; //El booleano Inverted Mouse, será el que en el siguiente bloque de código invierta la dirección del mouse.
-        
+	float mouseY;
+	float sensitivity = 100.0f;
     void Update()
     {
-        mouseX += Input.GetAxis("Mouse X"); // Con el Input, obtenemos la ubicación en X del mouse.
-
-        if (InvertedMouse) // Al utlizar un If, con el booleano Inverted Mouse, podemos obtener la ubicación del mouse en el eje Y, tanto en dirección posotiva como negativa.
-        {
-            mouseY += Input.GetAxis("Mouse Y"); // Con el Input de MouseY, obtenemos la ubicación en el eje Y del mouse, pero de dirección positiva. 
-        }
-        else
-        {
-            mouseY -= Input.GetAxis("Mouse Y"); // Al haber utilizado el Inverted Mouse, podemos averiguar la posición del mouse en Y, pero en dirección negativa.
-        }
-
-        transform.eulerAngles = new Vector3(mouseY, mouseX, 0); // Se creó un Vector3, que almacena el eje X y Y de la cámara.
+        CameraRot();
 
     }
+
+	public void CameraRot()
+	{
+		 mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+		 mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+		
+		transform.Rotate(-transform.right * mouseY);
+		
+	}
 }
