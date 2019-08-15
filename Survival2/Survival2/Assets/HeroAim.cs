c0d876074b5ee10d4d2da88d8cb512b1798f2751
Salomé Pelaez <spsalome@gmail.com>
2019-08-15 16:24:22 -0500

    Fixes the camera rotation

--- Survival2/Survival2/Assets/HeroAim.cs
+++ Survival2/Survival2/Assets/HeroAim.cs
@@ -8,12 +8,13 @@ public class HeroAim : MonoBehaviour
     float mouseX;
     float mouseY;
     float sensitivity = 40.0f;
-
+    float xAxisClamp = 0.0f;
 
     void Update()
     {
         mouseX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        
+        xAxisClamp = mouseY;
+
         if (InvertedMouse)
         {
             mouseY += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
@@ -23,53 +24,20 @@ public class HeroAim : MonoBehaviour
             mouseY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         }
 
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            
+        }
+
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+           
+        }
+
         transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
     }
-} //Funciona pero da una vuelta completa xdd
-/*
-{
-    float mouseX;
-float mouseY;
-float sensitivity = 40.0f;
-float xAxisClamp = 0.0f;
-
-void Update()
-{
-    CameraRot();
-}
-
-public void CameraRot()
-{
-    mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-    mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-
-    xAxisClamp += mouseY;
-
-    if (xAxisClamp > 90.0f)
-    {
-        xAxisClamp = 90.0f;
-        mouseY = 0.0f;
-        ClampXAxis(270.0f);
-    }
-
-    else if (xAxisClamp < -90.0f)
-    {
-        xAxisClamp = -90.0f;
-        mouseY = 0.0f;
-        ClampXAxis(90.0f);
-    }
-
-    transform.Rotate(Vector3.left * mouseY);
-    transform.Rotate(Vector3.up * mouseX);
-
-
-}
-
-void ClampXAxis(float myX)
-{
-    Vector3 eulerRotation = transform.eulerAngles;
-    eulerRotation.x = myX;
-    transform.eulerAngles = eulerRotation;
-
-}
-}*/
+} 
